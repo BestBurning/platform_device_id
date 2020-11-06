b0fdc733c5184881e64a225db1dafc16253f5bfc
Shea <zhushuai026@gmail.com>
2020-11-06 16:26:21 +0800

    update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.2
+upgrade dependency
+
 ## 0.1.1
 fix bug for go-flutter in linux
 
--- lib/platform_device_id.dart
+++ lib/platform_device_id.dart
@@ -3,14 +3,15 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:device_info/device_info.dart';
 
-
+/// Provides device id information.
 class PlatformDeviceId {
+  /// MethodChannel
   static const MethodChannel _channel =
       const MethodChannel('com.di1shaui.flutter/platform_device_id');
-
+  /// Provides device and operating system information.
   static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
 
-
+  /// Information derived from `android`-`androidId` or `ios`-`identifierForVendor`
   static Future<String> get getDeviceId async {
 
     String deviceId;
