22d72cf392171976399d825ccc8ee0c9e88a6a85
Shea <zhushuai026@gmail.com>
2021-04-05 06:57:32 +0800

    platform_device_id_windows format

--- platform_device_id_windows/example/test/widget_test.dart
+++ platform_device_id_windows/example/test/widget_test.dart
@@ -18,9 +18,10 @@ void main() {
     // Verify that platform version is retrieved.
     expect(
       find.byWidgetPredicate(
-        (Widget widget) => widget is Text && 
-                            widget.data != null &&
-                           widget.data!.startsWith('Device ID:'),
+        (Widget widget) =>
+            widget is Text &&
+            widget.data != null &&
+            widget.data!.startsWith('Device ID:'),
       ),
       findsOneWidget,
     );
